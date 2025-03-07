4b302eec3ef7d77058b66579cea2e871e48f7b86
thierry-gx <2359280@tongji.edu.cn>
2025-03-07 17:00:17 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,26 +5,32 @@ N = 6
 signs = ['+', '-', '*', '/']
 
 def main():
-    for i in range (N):
-        numbers = []
-        operators = []
-        num = randint(3, 5)
-        for j in range (num):
-            numbers.append(randint(0, 100))
-        for j in range (num - 1):
-            operators.append(choice(signs))
-        result(numbers, operators)
+    with open('results.txt', 'w') as fout:
+        fout.write("2359280" + '\n')
+
+        for i in range (N):
+            numbers = []
+            operators = []
+            num = randint(3, 5)
+            for j in range (num):
+                numbers.append(randint(0, 100))
+            for j in range (num - 1):
+                operators.append(choice(signs))
+            result(numbers, operators, fout)
         
 
-def result(numbers, operators):
+def result(numbers, operators, fout):
     expression = str(numbers[0])
     for num, op in zip(numbers[1:], operators):
         expression += f'{op}{num}'
     print(expression, end = "")
     res = eval(expression)
     if isinstance(res, int):
-        print(f"={res}")
+       output = f"={res}"
     else:
-        print(f"={res:.2f}")
+        output = f"={res:.2f}"
+    
+    print(output)
+    fout.write(output + '\n')
 
 main()
